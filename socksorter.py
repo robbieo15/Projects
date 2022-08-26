809f82d9d306b4e4a036946bb2f0a917abb14cfb
Land of Yeti <68252306+robbieo15@users.noreply.github.com>
2022-08-25 20:15:31 -0400

    Add files via upload

--- socksorter.py
+++ socksorter.py
@@ -1,14 +1,14 @@
 import random
-from sys import maxsize
 
 # Create a function to bucket the socks/counter at random
+
 def sock_counter():
 
     sock_list = [0,0,0,0]
     sock_counter = 0
-   
+
     while sock_counter < 100:
-            sock_list[:] = [item + random.randint(1,25) for item in sock_list]
+            sock_list[:] = [item + random.randint(1,10) for item in sock_list]
             sock_counter = sum(sock_list)
 
     print(sock_list)
@@ -16,9 +16,9 @@ def sock_counter():
 
     max_sock = sock_list.index(max(sock_list))
     print(f"The index for the max sock is {max_sock}")
-    
+
     if sock_counter > 100:
-        sock_list[max_sock] = sock_list[max_sock] - (sock_counter-100)
+        sock_list[max_sock] = abs(sock_list[max_sock] - (sock_counter-100))
         sock_counter = sum(sock_list)
 
     print(f"Deleting the {max_sock} index by the difference between sum of the list and 100 brings the total socks to {sock_counter}")
@@ -30,19 +30,19 @@ def sock_counter():
         'thigh' : sock_list[3]
     }
 
-    matches = 0
     loners = 0
     sock_values = list(sock_types.values())
+    print(sock_values)
 
-    while matches < 50:
-        for item in sock_values:
-            if item% 2 != 0:
-                item - 1
-                loners += 1
-            else:
-                matches += item/2
-            print(matches)
-            print(loners)
+    for item in sock_types.values():
+        if item % 2 != 0:
+            loners += 1
+
+    matches = (50 - loners)
+
+    print(matches)
+    print(loners)
+
 
 sock_counter()
 

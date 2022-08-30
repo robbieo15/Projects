4c28213676d7e3bf85c3dfd08dc5ab3eecac18fe
Land of Yeti <68252306+robbieo15@users.noreply.github.com>
2022-08-30 19:04:08 -0400

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -2,30 +2,42 @@ class ATM:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transactions = []
 
     def check_balance(self, name):
         # Returns account balance
-        return self.balance[name]
+        return self.balance
 
-    def deposit(self, amount_deposit):
+    def deposit(self, amount):
         # deposits he given amount to the account
-        self.balance = amount_deposit + self.balance
+        self.balance = amount + self.balance
+        self.transactions.append(f"Deposited ${amount}")
+        return self.balance
 
-    def check_withdrawal(self, _withdraw):
+    def check_withdrawal(self, withdraw):
         # returns true if the withdrawn amount won't put the account in the negative
-        if balance - amount > 0:
+        if self.balance - withdraw >= 0:
             return True
 
-    def withdraw(self, amount_withdraw):
+    def withdraw(self, amount):
         # withdraws the amount from the account and returns the amount
-        self.balance = balance - amount_withdraw
+        self.balance = self.balance - amount
+        self.transactions.append(f"Withdrew ${amount}")
+        return self.balance
 
     def calc_interest(self):
         # returns the amount of interest calculated on the account
-        s
+        return self.balance*.1
 
+    def print_transactions(self):
+        #returns every transaction made
+        for counter,amount in enumerate(self.transactions):
+            print(counter+1, amount)
+            
 
-atm = ATM()  # create an instance of our class
+name = input('What is the name of your new wallet: ')
+balance = int(input('Please set your starting balance: '))
+atm = ATM(name,balance)  # create an instance of our class
 print('Welcome to the ATM')
 
 menu_options = {
@@ -33,7 +45,8 @@ menu_options = {
     '2': 'Deposit',
     '3': 'Withdraw',
     '4': 'Interest',
-    '5': 'Exit'
+    '5': 'Transactions',
+    '6': 'Exit'
 }
 
 while True:
@@ -46,7 +59,7 @@ while True:
     command = menu_options.get(command)
 
     if command == 'Balance':
-        balance = atm.check_balance()  # call the check_balance() method
+        balance = atm.check_balance(name)  # call the check_balance() method
         print(f'Your balance is ${balance}')
 
     elif command == 'Deposit':
@@ -71,6 +84,9 @@ while True:
         amount = atm.calc_interest()  # call the calc_interest() method
         atm.deposit(amount)
         print(f'Accumulated ${amount} in interest')
+
+    elif command == 'Transactions':
+        atm.print_transactions()
 
     elif command == 'Exit':
         print("Goodbye!")
